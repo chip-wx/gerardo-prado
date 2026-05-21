ef69e375190f698fe62c84fc371cbde5a92bcb78
Gerardo <gerardopalonzo@gmail.com>
2026-05-21 15:10:48 -0600

    V 1.0.0

---
+++ src/components/js/stackSVG.js
@@ -0,0 +1,33 @@
+import react from "../../assets/svg/react.svg";
+import astro from "../../assets/svg/astro.svg";
+import ts from "../../assets/svg/ts.svg";
+import js from "../../assets/svg/js.svg";
+import css from "../../assets/svg/css.svg";
+import html from "../../assets/svg/html.svg";
+import node from "../../assets/svg/node.svg";
+import express from "../../assets/svg/express.svg";
+import mysql from "../../assets/svg/mysql.svg";
+
+const reactSVG = { src: react, alt: "react" };
+const astroSVG = { src: astro, alt: "astro" };
+const tsSVG = { src: ts, alt: "typescript" };
+const jsSVG = { src: js, alt: "javascript" };
+const cssSVG = { src: css, alt: "css" };
+const htmlSVG = { src: html, alt: "html" };
+const nodeSVG = { src: node, alt: "node" };
+const expressSVG = { src: express, alt: "express" };
+const mysqlSVG = { src: mysql, alt: "mysql" };
+
+const stackSVGs = [
+  reactSVG,
+  astroSVG,
+  tsSVG,
+  jsSVG,
+  cssSVG,
+  htmlSVG,
+  nodeSVG,
+  expressSVG,
+  mysqlSVG,
+];
+
+export { stackSVGs };
